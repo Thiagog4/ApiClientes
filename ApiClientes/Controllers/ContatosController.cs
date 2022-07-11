c0658f77a9177b2a1df24b6d04aa24ac3a70fa99
Thiagog4 <thiagojoseguedes@hotmail.com>
2022-07-11 11:55:28 -0300

    alterado

--- ApiClientes/Controllers/ContatosController.cs
+++ ApiClientes/Controllers/ContatosController.cs
@@ -50,6 +50,8 @@ namespace ApiClientes.Controllers
             return contatos;
         }
 
+
+
         // PUT: api/Contatos/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
